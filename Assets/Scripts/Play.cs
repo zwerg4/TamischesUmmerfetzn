39a49001169de3c91753b2b6340673bb3ce0df04
zwerg4 <fab.gan@hotmail.de>
2020-06-17 17:42:18 +0200

    tamisches umerfetzn beta 1.01

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+	
+	public void LoadMenu()
+    {
+		SceneManager.LoadScene("StartScene");
+    }
+	
+	public void PlayTrack1()
+    {
+		SceneManager.LoadScene("Track1");
+    }
+}
